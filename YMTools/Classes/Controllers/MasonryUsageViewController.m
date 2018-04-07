92ba25fc9559295720a5818dc18e69f5564d1e49
YM <longxian@longxiandeMacBook-Pro.local>
2018-04-07 18:18:27 +0800

    no message

--- YMTools/Classes/Controllers/MasonryUsageViewController.m
+++ YMTools/Classes/Controllers/MasonryUsageViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "MasonryUsageViewController.h"
-
+#import "YMMasonryView.h"
 /**
  更新约束和布局，主要用以下四个API：
  - (void)updateConstraintsIfNeeded  调用此方法，如果有标记为需要重新布局的约束，则立即进行重新布局，内部会调用updateConstraints方法
@@ -27,20 +27,31 @@
 /** UILabel */
 @property (strong, nonatomic)UILabel *label;
 
+/**  */
+@property (strong, nonatomic)YMMasonryView *masonryView;
+
 @end
 
 @implementation MasonryUsageViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+  
+    [self showMasonryView];
+    
+    
+    
+}
+
+- (void)base {
     self.cyanView = [UIView new];
     self.cyanView.backgroundColor = [UIColor cyanColor];
     [self.view addSubview:self.cyanView];
     
     //设置内边距
-//    [self.cyanView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.edges.equalTo(self.view).insets(UIEdgeInsetsMake(74, 355, 10, 10));
-//    }];
+    //    [self.cyanView mas_makeConstraints:^(MASConstraintMaker *make) {
+    //        make.edges.equalTo(self.view).insets(UIEdgeInsetsMake(74, 355, 10, 10));
+    //    }];
     
     // 更新约束
     // 设置center和size，这样就可以达到简单进行约束的目的
@@ -48,33 +59,33 @@
     self.greenView.backgroundColor = [UIColor greenColor];
     [self.view addSubview:self.greenView];
     
-//    [self.greenView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.center.equalTo(self.view);
-//        make.size.mas_equalTo(CGSizeMake(300, 300));
-//    }];
-//
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        // 指定更新size，其他约束不变。
-//        [self.greenView mas_updateConstraints:^(MASConstraintMaker *make) {
-//            // 设置约束比例
-//            make.width.equalTo(self.view).multipliedBy(0.1);
-//        }];
-//    });
+    //    [self.greenView mas_makeConstraints:^(MASConstraintMaker *make) {
+    //        make.center.equalTo(self.view);
+    //        make.size.mas_equalTo(CGSizeMake(300, 300));
+    //    }];
+    //
+    //    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    //        // 指定更新size，其他约束不变。
+    //        [self.greenView mas_updateConstraints:^(MASConstraintMaker *make) {
+    //            // 设置约束比例
+    //            make.width.equalTo(self.view).multipliedBy(0.1);
+    //        }];
+    //    });
     
-//    self.label = [UILabel new];
-//    self.label.numberOfLines = 0;
-//    self.label.backgroundColor = [UIColor lightGrayColor];
-//    [self.view addSubview:self.label];
-//
-//    [self.label mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.center.equalTo(self.view);
-//        //设置宽度小于等于200
-//        make.width.lessThanOrEqualTo(@200);
-//        // 设置高度小于定于10
-//        make.height.greaterThanOrEqualTo(@10);
-//    }];
-//
-//    self.label.text = @"我没有很想你，只是在早上醒来时，看看有没有你发来信息和未接来电；我没有很想你，只是把你来电调成唯一的铃音；我没有很想你，只是在听歌时，被某句歌词击中，脑中出现短暂的空白；我没有很想你，只是想看看你的样子，听听你的声音；我又没有很想你，只是每次醒来时，第一个想到你。";
+    //    self.label = [UILabel new];
+    //    self.label.numberOfLines = 0;
+    //    self.label.backgroundColor = [UIColor lightGrayColor];
+    //    [self.view addSubview:self.label];
+    //
+    //    [self.label mas_makeConstraints:^(MASConstraintMaker *make) {
+    //        make.center.equalTo(self.view);
+    //        //设置宽度小于等于200
+    //        make.width.lessThanOrEqualTo(@200);
+    //        // 设置高度小于定于10
+    //        make.height.greaterThanOrEqualTo(@10);
+    //    }];
+    //
+    //    self.label.text = @"我没有很想你，只是在早上醒来时，看看有没有你发来信息和未接来电；我没有很想你，只是把你来电调成唯一的铃音；我没有很想你，只是在听歌时，被某句歌词击中，脑中出现短暂的空白；我没有很想你，只是想看看你的样子，听听你的声音；我又没有很想你，只是每次醒来时，第一个想到你。";
     
     
     
@@ -83,21 +94,21 @@
     self.blueView.backgroundColor = [UIColor blueColor];
     [self.view addSubview:self.blueView];
     
-//    CGFloat padding = 74.f;
-//    [self.cyanView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.right.top.equalTo(self.view).insets(UIEdgeInsetsMake(padding, padding, 0, padding));
-//        make.bottom.equalTo(self.greenView.mas_top).offset(-padding);
-//    }];
-//
-//    [self.greenView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.right.equalTo(self.view).insets(UIEdgeInsetsMake(0, padding, 0, padding));
-//        make.bottom.equalTo(self.blueView.mas_top).offset(-padding);
-//    }];
-//
-//    [self.blueView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.right.bottom.equalTo(self.view).insets(UIEdgeInsetsMake(0, padding, padding, padding));
-//        make.height.equalTo(@[self.cyanView, self.greenView]);
-//    }];
+    //    CGFloat padding = 74.f;
+    //    [self.cyanView mas_makeConstraints:^(MASConstraintMaker *make) {
+    //        make.left.right.top.equalTo(self.view).insets(UIEdgeInsetsMake(padding, padding, 0, padding));
+    //        make.bottom.equalTo(self.greenView.mas_top).offset(-padding);
+    //    }];
+    //
+    //    [self.greenView mas_makeConstraints:^(MASConstraintMaker *make) {
+    //        make.left.right.equalTo(self.view).insets(UIEdgeInsetsMake(0, padding, 0, padding));
+    //        make.bottom.equalTo(self.blueView.mas_top).offset(-padding);
+    //    }];
+    //
+    //    [self.blueView mas_makeConstraints:^(MASConstraintMaker *make) {
+    //        make.left.right.bottom.equalTo(self.view).insets(UIEdgeInsetsMake(0, padding, padding, padding));
+    //        make.height.equalTo(@[self.cyanView, self.greenView]);
+    //    }];
     
     
     // 子视图垂直居中练习
@@ -122,19 +133,20 @@
         view.backgroundColor = [UIColor blueColor];
         view;
     });
-    
-    
-    
-    
 }
 
 
+- (void)showMasonryView {
+    [self.view addSubview:self.masonryView];
+}
 
-
-
-
-
-
+-(YMMasonryView *)masonryView {
+    if (!_masonryView) {
+        _masonryView = [[YMMasonryView alloc] initWithFrame:CGRectMake(0, 74, MainScreenWidth, MainScreenHeight - 49 - 74)];
+        _masonryView.backgroundColor = [UIColor lightGrayColor].flatten;
+    }
+    return _masonryView;
+}
 
 
 
